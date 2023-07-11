f76794b1c368fadb49151bad0e5650fbaff67f55
alyuoncha <al.zotikova@gmail.com>
2023-07-11 13:59:19 +0300

    patronus

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,13 @@
 import '@/app/index.scss'
+// core version + navigation, pagination modules:
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
+const swiper = new Swiper('.swiper');
+
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("menu-panel").addEventListener("click", function() {
         document.querySelector("header").classList.toggle("open")
