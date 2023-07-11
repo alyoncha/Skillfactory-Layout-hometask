3baaf9e65a7ff194167e717e5461f971f10e0e9d
alyuoncha <al.zotikova@gmail.com>
2023-07-11 11:38:07 +0300

    avadakedavra

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,11 @@
 import '@/app/index.scss'
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("menu-panel").addEventListener("click", function() {
+        document.querySelector("header").classList.toggle("open")
+    })
+})
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("menu-panel-close").addEventListener("click", function() {
+        document.querySelector("header").classList.toggle("open")
+    })
+})
